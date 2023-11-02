1c076c086cb39391ba1dbe44ab71064d8a5aa238
Lakshay <lakshay.sbp@gmail.com>
2023-11-02 13:51:41 +0530

    NextUth connected with GithubProvider

--- gdrive-clone/src/pages/index.tsx
+++ gdrive-clone/src/pages/index.tsx
@@ -1,12 +1,10 @@
-import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
-
-import { api } from "@/utils/api";
+import Button from "@/components/Button";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
+  const { data: session } = useSession();
   return (
     <>
       <Head>
@@ -19,33 +17,9 @@ export default function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Creaaate <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
-        
+          <Button onClick={() => signIn()} btnClass="" title="Sign Up" />
         </div>
       </main>
     </>
-  );
-}
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
   );
 }
